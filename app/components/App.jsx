e3948c779fb79fff3f25384b0ba2109eed41043d
Max Ehnert <max.ehnert@gmail.com>
2016-01-08 19:06:29 -0600

    Add, Edit, Delete complete

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,65 @@
+import uuid from 'node-uuid';
 import React, { Component } from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
 export default class App extends Component {
-  render() {
-    return <Note />;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn react'
+        },
+        {
+          id: uuid.v4(),
+          task: 'laern nada'
+        }
+      ]
+    };
   }
+  render() {
+
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes
+          notes={notes}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  };
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if (note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
+  };
+  deleteNote = (id) => {
+    this.setState({
+      notes: this.state.notes.filter((note) => note.id !== id)
+    });
+  };
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <div>Learn Webpack</div>;
+export default class Note extends Component {
+  constructor(props) {
+    super(props);
+
+    // Tracks the editing state
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+
+    // Render the component differently based on state
+    if (this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+
+    // Deal with blur and nput handlers. This map to DOM events
+    return <input type="text"
+      autoFocus={true}
+      placeholder={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderNote = () => {
+
+    const onDelete = this.props.onDelete;
+
+    // If the user clicks a normal note, trigger editing logic
+    return (
+      <div onClick={this.edit}>
+        <span>{this.props.task}</span>
+        { onDelete ? this.renderDelete() : null }
+        </div>
+      );
+  };
+  renderDelete = () => {
+    return <button onClick={this.props.onDelete}>x</button>;
+  };
+  edit = () => {
+
+    // Enter edit mode
+    this.setState({
+      editing: true
+    });
+  };
+  finishEdit = (e) => {
+    if (this.props.onEdit) {
+      this.props.onEdit(e.target.value);
+    }
+
+    // Exit edit mode
+    this.setState({
+      editing: false
+    });
+  };
+}
