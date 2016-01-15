5333c4a4f3e718455ca4fe2d33cbce2a49471a4d
Max Ehnert <max.ehnert@gmail.com>
2016-01-14 20:06:42 -0600

    added react-dnd

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,64 +1,86 @@
 import React, { Component } from 'react';
+import { DragSource } from 'react-dnd';
+import ItemTypes from '../constants/ItemTypes';
 
-export default class Note extends Component {
-  constructor(props) {
-    super(props);
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging a note ', props);
 
-    // Tracks the editing state
-    this.state = {
-      editing: false
-    };
+    return {};
   }
-  render() {
-
-    // Render the component differently based on state
-    if (this.state.editing) {
-      return this.renderEdit();
-    }
-
-    return this.renderNote();
-  }
-  renderEdit = () => {
-
-    // Deal with blur and nput handlers. This map to DOM events
-    return <input type="text"
-      autoFocus={true}
-      placeholder={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-  renderNote = () => {
-
-    const onDelete = this.props.onDelete;
-
-    // If the user clicks a normal note, trigger editing logic
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-        { onDelete ? this.renderDelete() : null }
-        </div>
-      );
-  };
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  };
-  edit = () => {
-
-    // Enter edit mode
-    this.setState({
-      editing: true
-    });
-  };
-  finishEdit = (e) => {
-    if (this.props.onEdit) {
-      this.props.onEdit(e.target.value);
-    }
-
-    // Exit edit mode
-    this.setState({
-      editing: false
-    });
-  };
 }
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+export default class Note extends Component {
+  render() {
+    const {connectDragSource, id, onMove, ...props} = this.props;
+
+    return connectDragSource(
+      <li {...this.props}>{this.props.children}</li>
+    );
+  }
+}
+
+// export default class Note extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     // Tracks the editing state
+//     this.state = {
+//       editing: false
+//     };
+//   }
+//   render() {
+//
+//     // Render the component differently based on state
+//     if (this.state.editing) {
+//       return this.renderEdit();
+//     }
+//
+//     return this.renderNote();
+//   }
+//   renderEdit = () => {
+//
+//     // Deal with blur and nput handlers. This map to DOM events
+//     return <input type="text"
+//       autoFocus={true}
+//       placeholder={this.props.task}
+//       onBlur={this.finishEdit}
+//       onKeyPress={this.checkEnter} />;
+//   };
+//   renderNote = () => {
+//
+//     const onDelete = this.props.onDelete;
+//
+//     // If the user clicks a normal note, trigger editing logic
+//     return (
+//       <div onClick={this.edit}>
+//         <span className="task">{this.props.task}</span>
+//         { onDelete ? this.renderDelete() : null }
+//         </div>
+//       );
+//   };
+//   renderDelete = () => {
+//     return <button
+//       className="delete-note"
+//       onClick={this.props.onDelete}>x</button>;
+//   };
+//   edit = () => {
+//
+//     // Enter edit mode
+//     this.setState({
+//       editing: true
+//     });
+//   };
+//   finishEdit = (e) => {
+//     if (this.props.onEdit) {
+//       this.props.onEdit(e.target.value);
+//     }
+//
+//     // Exit edit mode
+//     this.setState({
+//       editing: false
+//     });
+//   };
+// }
